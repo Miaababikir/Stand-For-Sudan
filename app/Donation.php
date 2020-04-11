c0e7876ee28da4755a1c317845c7f2e18e39a66e
Mosab Ibrahim <miaababikir@gmail.com>
2020-04-11 09:21:51 +0200

    Adding Donation to API

--- app/Donation.php
+++ app/Donation.php
@@ -9,8 +9,18 @@ class Donation extends Model
 {
     protected $guarded = [];
 
+    protected $appends = ['donation'];
+
     public function getCreatedAtAttribute($value)
     {
         return Carbon::parse($value)->diffForHumans();
+    }
+
+    public function getDonationAttribute()
+    {
+        if ($this->id - 1 === 0)
+            return $this->total_amount;
+
+        return $this->total_amount - Donation::find($this->id - 1)->total_amount;
     }
 }
